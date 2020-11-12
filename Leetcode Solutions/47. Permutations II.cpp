e38560f88854228a5ca3df49f2a21b4748828511
shahin <shahincsejnu@gmail.com>
2020-11-12 20:33:04 +0600

    updated

---
+++ Leetcode Solutions/47. Permutations II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> ans;
+        
+        do{
+            ans.push_back(nums);    
+        }while(next_permutation(nums.begin(), nums.end()));
+        
+        return ans;
+    }
+};
