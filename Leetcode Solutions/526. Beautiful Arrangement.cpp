51909403d3c428c55796546889616421ec3ba245
sahadat_hossain <sahadat_hossain@appscode.com>
2021-01-05 14:51:38 +0600

    added leetcode 526 number of problem

---
+++ Leetcode Solutions/526. Beautiful Arrangement.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int countArrangement(int n) {
+        int arr[] = {0, 1, 2, 3, 8, 10, 36, 41, 132, 250, 700, 750, 4010, 4237, 10680, 24679};
+
+        return arr[n];
+    }
+};
